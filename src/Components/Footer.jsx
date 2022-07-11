12ce1eb1117b55568e93ed3dce6681d2ce9cf850
Lucas <bazezimo42@gmail.com>
2022-07-11 06:53:24 -0300

    footer implemented

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AiFillCaretUp } from "react-icons/ai";
+import { BsFacebook, BsInstagram } from 'react-icons/bs'
+import {
+  Foot,
+  FooterBox,
+  GoTop,
+  Socials,
+} from "./StyledComponentFooter";
+
+
+export default function Footer() {
+
+  const scrollTop = () =>{
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  };
+
+  return (
+    <FooterBox>
+      <GoTop onClick={scrollTop}>
+        <AiFillCaretUp />
+        <p>Voltar ao topo da página</p>
+      </GoTop>
+      <Socials>
+          <a href="http://www.instagram.com"><BsInstagram />Instagram</a>
+          <a href="http://www.facebook.com"><BsFacebook />FaceBook</a>
+          <p>Preços e condições de pagamento exclusivos para compras via internet, podendo variar nas lojas físicas. Ofertas válidas na compra de até 10 peças de cada produto por cliente, até o término dos nossos estoques para internet. Caso os produtos apresentem divergências de valores, o preço válido é o da sacola de compras. Vendas sujeitas a análise e confirmação de dados.</p>
+      </Socials>
+    </FooterBox>
+  );
+}
--- src/Components/StyledComponentUser.jsx
+++ src/Components/StyledComponentUser.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Conteiner = styled.div`
     display: grid;
-    margin: 10px auto 0 auto;
+    margin: 10px auto 20px auto;
     grid-template-columns: 50% 50%;
     grid-template-rows: 2;
     grid-gap: 5px;
